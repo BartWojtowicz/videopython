2078587b1dbe9bf376dc92e45b62e57231c198b7
Bartosz Rudnikowicz <bartoszrud@Bartoszs-MacBook-Pro-5.local>
2024-07-28 20:22:28 +0200

    Adding mypy stubs

--- src/typehints/transformers/models.pyi
+++ src/typehints/transformers/models.pyi
@@ -12,3 +12,34 @@ class AutoTokenizer:
     @classmethod
     def from_pretrained(cls, model_type: str) -> AutoTokenizer: ...
     def __call__(self, text: str, return_tensors: str) -> torch.Tensor: ...
+
+class PretrainedConfig:
+    def __init__(self, **kwargs): ...
+
+class EncodecConfig(PretrainedConfig):
+    sampling_rate: int
+
+class MusicgenDecoderConfig(PretrainedConfig):
+    pass
+
+class MusicgenConfig:
+    @classmethod
+    def from_sub_models_config(
+        cls,
+        text_encoder_config: PretrainedConfig,
+        audio_encoder_config: PretrainedConfig,
+        decoder_config: MusicgenDecoderConfig,
+        **kwargs,
+    ) -> MusicgenConfig: ...
+    audio_encoder: EncodecConfig
+
+class MusicgenForConditionalGeneration:
+    @classmethod
+    def from_pretrained(cls, model_type: str) -> MusicgenForConditionalGeneration: ...
+    def generate(self, **inputs) -> torch.Tensor: ...
+    config: MusicgenConfig
+
+class AutoProcessor:
+    @classmethod
+    def from_pretrained(cls, model_type: str) -> AutoProcessor: ...
+    def __call__(self, text: list, padding: bool, return_tensors: str) -> dict: ...
