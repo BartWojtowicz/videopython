bf63bb6fbdd2bab983302ef4c3e42354d8e6a03d
Bartosz Rudnikowicz <bartoszrud@Bartoszs-MacBook-Pro-5.local>
2024-07-28 14:46:05 +0200

    Problem with pydub fixed

--- src/videopython/generation/audio.py
+++ src/videopython/generation/audio.py
@@ -26,6 +26,10 @@ class TextToSpeech:
 
 class TextToMusic:
     def __init__(self) -> None:
+        """
+        Generates music from text using the Musicgen model.
+        Check the license for the model before using it.
+        """
         self.processor = AutoProcessor.from_pretrained(MUSIC_GENERATION_MODEL_SMALL)
         self.model = MusicgenForConditionalGeneration.from_pretrained(MUSIC_GENERATION_MODEL_SMALL)
 
@@ -37,25 +41,12 @@ class TextToMusic:
         )
         audio_values = self.model.generate(**inputs, max_new_tokens=max_new_tokens)
         sampling_rate = self.model.config.audio_encoder.sampling_rate
-        numpy_audio = audio_values[0, 0].numpy()
-        output = (audio_values[0, 0].T.float().numpy() * (2**31 - 1)).astype(np.int32)
-        scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=numpy_audio)
+        output = (audio_values[0].T.float().numpy() * (2**31 - 1)).astype(np.int32)
 
-        # TODO: Pydub saves completely different music 
         audio = AudioSegment(
-            data=output.tobytes(),
+            data=output,
             frame_rate=sampling_rate,
             sample_width=4,
             channels=1,
         )
         return audio
-
-
-if __name__ == "__main__":
-    tm = TextToMusic()
-    audio = tm.generate_audio("A happy song", max_new_tokens=256)
-    audio.export("test_fb.wav", format="wav")
-
-    # Something completely different gets saved than loaded!
-    test = AudioSegment.from_file("musicgen_out.wav")
-    test.export("test_fb2.wav", format="wav")
