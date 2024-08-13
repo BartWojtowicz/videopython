27cb472f112387468736dc0ea8c781cb887de263
Bartosz Wójtowicz <bartoszwojtowicz@outlook.com>
2024-08-13 18:55:58 +0200

    Allow multiple video formats (#88)
    Add support for multiple video formats.

--- src/typehints/cv2/constants.pyi
+++ src/typehints/cv2/constants.pyi
@@ -12,3 +12,7 @@ INTER_LINEAR: Final[int]
 INTER_NEAREST: Final[int]
 INTER_CUBIC: Final[int]
 INTER_LANCZOS4: Final[int]
+
+# Color properties
+COLOR_RGB2BGR: Final[int]
+COLOR_BGR2RGB: Final[int]
--- src/typehints/cv2/functions.pyi
+++ src/typehints/cv2/functions.pyi
@@ -1,4 +1,4 @@
-from typing import TypeVar
+from typing import TypeVar, overload
 
 import numpy as np
 import numpy.typing as npt
@@ -24,3 +24,8 @@ def GaussianBlur(
     sigmaY: float = ...,
     borderType: int = ...,
 ) -> npt.NDArray[_TImg]: ...
+def imwrite(filename: str, img: npt.NDArray[_TImg]) -> bool: ...
+@overload
+def cvtColor(src: npt.NDArray[np.uint8], code: int) -> npt.NDArray[np.uint8]: ...
+@overload
+def cvtColor(src: npt.NDArray[np.float64], code: int) -> npt.NDArray[np.float64]: ...
