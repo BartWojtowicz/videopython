a8aa89bdd512db041325c43e074fe1016edc7fba
Bartosz Rudnikowicz <bartoszrud@Bartoszs-MacBook-Pro-5.fritz.box>
2025-04-04 22:28:51 +0200

    transcription with whisper

---
+++ src/videopython/understanding/transcription.py
@@ -0,0 +1,35 @@
+from typing import Literal
+
+import numpy as np
+import whisper
+
+from videopython.base.video import Video
+
+
+class Transcription:
+    def __init__(self, model_name: Literal["tiny", "base", "small", "medium", "large", "turbo"] = "small") -> None:
+        self.model = whisper.load_model(name=model_name)
+
+    def transcribe_video(self, video: Video) -> dict:
+        """Transcribes video to text.
+
+        Args:
+            video: Video to transcribe.
+
+        Returns:
+            Transcribed text.
+        """
+
+        audio = video.audio.to_mono()
+        audio = audio.resample(whisper.audio.SAMPLE_RATE)
+        audio_data = audio.data
+
+        transcription = self.model.transcribe(audio_data, word_timestamps=True)
+
+        result = [
+            {"start": segment["start"], "end": segment["end"], "text": segment["text"]}
+            for segment in transcription["segments"]
+        ]
+
+
+        return result
